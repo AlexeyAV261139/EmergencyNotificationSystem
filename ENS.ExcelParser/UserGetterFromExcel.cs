3c4f3c84bc144150845003ec7de31828a31a5100
Alexey <Alexey261139@mail.ru>
2024-03-19 17:06:32 +0300

    Рефакторинг

--- ENS.ExcelParser/UserGetterFromExcel.cs
+++ ENS.ExcelParser/UserGetterFromExcel.cs
@@ -13,55 +13,75 @@ namespace ENS.ExcelParser
         {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
             _filePath = filePath;
-        }       
+        }
 
         public IEnumerable<User> GetUsers()
-        {           
-            return GetUsersFromDataTable();
-        }       
-
-        private IEnumerable<User> GetUsersFromDataTable()
         {
             var dataTables = GetDataTablesFromExcel();
-
-            foreach(DataTable dt in dataTables)
-            {
-                foreach(DataRow dataRow in dt.Rows)
-                {
-                    var cells = dataRow.ItemArray;
-
-                    var columns = dt.Columns;
-
-                    if (cells[0] != null)
-                    {
-                        User user = new User()
-                        {
-                            Name = cells[0]!.ToString()!
-                        };
-                        for (int i = 1; i < cells.Length; i++)
-                        {
-                            user.Contacts.Add(new Contact
-                            {
-                                Name = columns[i].ColumnName,
-                                Value = cells[i]!.ToString()!
-                            });
-                        }
-                        yield return user;
-                    }                    
-                }
-            }
+            return GetUsersFromDataTables(dataTables);
         }
 
         private DataTableCollection GetDataTablesFromExcel()
         {
             using (var stream = File.Open(_filePath, FileMode.Open, FileAccess.Read))
-            {                
+            {
                 using (var reader = ExcelReaderFactory.CreateReader(stream))
                 {
                     var dataTables = reader.AsDataSet().Tables;
                     return dataTables;
                 }
             }
+        }
+
+        private IEnumerable<User> GetUsersFromDataTables(DataTableCollection dataTables)
+        {
+            foreach (DataTable dt in dataTables)
+            {
+                for (int i = 1; i < dt.Rows.Count; i++)
+                {
+                    var dataRow = dt.Rows[i];
+                    string[] columnsNames = GetColumnsNames(dt);
+
+                    var user = GetUserFromRow(dataRow, columnsNames);
+                    if (user is null)
+                        continue;
+
+                    yield return user;
+                }                
+            }
+        }
+
+        private string[] GetColumnsNames(DataTable dt)
+        {
+
+
+        }
+
+        private User GetUserFromRow(DataRow dataRow, string[] columnsNames)
+        {
+            var cells = dataRow.ItemArray;
+
+            if (cells[0] is null)
+            {
+                throw new Exception("В строке не указан пользователь");
+            }
+
+            User user = new User()
+            {
+                Name = cells[0]!.ToString()!
+            };
+            for (int i = 1; i < cells.Length; i++)
+            {
+                if (cells[i] is null)
+                    continue;
+                user.Contacts.Add(new Contact
+                {
+                    Name = columnsNames[i],
+                    Value = cells[i]!.ToString()!
+                });
+            }
+            return user;
+
         }
     }
 }
